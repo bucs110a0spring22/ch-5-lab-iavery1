b43e4d0782de245d3bd13fb71f8b8b5cc552a2e9
Ian B B <98533883+iavery1@users.noreply.github.com>
2022-03-24 04:13:56 +0000

    code

--- README.md
+++ README.md
@@ -8,12 +8,12 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ Set up a dart board with my turtle and had two "players" throw darts.
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: < 1 >
 
-Grace days remaining: < # >/5
+Grace days remaining: < 4 >/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
  < What parts of the project you were not able to complete >
--- main.py
+++ main.py
@@ -24,6 +24,7 @@ Functions you must implement:
   playDarts(myturtle=None) - a simulated game of darts between two players
   montePi(myturtle=None, num_darts=0) - simulation algorithm returns the approximation of pi
 '''
+import math
 import turtle
 import random
 import time
@@ -32,8 +33,89 @@ import time
 #                   Your Code Goes Below                #
 #########################################################
 
+def drawSquare(myturtle=None, width=0, top_left_x=0, top_left_y=0):
+  myturtle.setworldcoordinates(top_left_x, top_left_y-width, top_left_x+width, top_left_y)
+  myturtle.bgcolor("green")
+  plane = turtle.Turtle()
+  plane.hideturtle()
+  plane.up()
+  plane.goto(top_left_x,top_left_y)
+  plane.down()
+  plane.goto(top_left_x, top_left_y-width)
+  plane.goto(top_left_x+width, top_left_y-width)
+  plane.goto(top_left_x+width, top_left_y)
+  plane.goto(top_left_x,top_left_y)
+  
+def drawLine(myturtle=None, x_start=-1, y_start=-1, x_end=1, y_end=1):
+  #myturtle.hideturtle()
+  myturtle.up()
+  myturtle.goto(x_start,0)
+  myturtle.down()
+  myturtle.goto(x_end,0)
+  myturtle.up()
+  myturtle.goto(0,y_start)
+  myturtle.down()
+  myturtle.goto(0,y_end)
 
+def drawCircle(myturtle=None, radius=0):
+  #myturtle.hideturtle()
+  myturtle.goto(0,-radius)
+  myturtle.circle(radius)
+  
+def setUpDartboard(myscreen=None, myturtle=None): 
+  drawSquare(myscreen, 2, -1, 1)
+  drawLine(myturtle, -1, 0, 1, 0)
+  drawLine(myturtle, 0, -1, 0, 1)
+  drawCircle(myturtle, 1)
 
+def isInCircle(myturtle=None, circle_center_x=0, circle_center_y=0, radius=0):
+  distance = myturtle.distance(circle_center_x,circle_center_y)
+  if distance < 1:
+    #print(distance)
+    return True
+  else:
+    #print(distance)
+    return False
+  
+def throwDart(myturtle=None):
+  x_coordinate = random.uniform(-1,1)
+  y_coordinate = random.uniform(-1,1)
+  myturtle.up()
+  myturtle.speed(0)
+  myturtle.goto(x_coordinate,y_coordinate)
+  if isInCircle(myturtle, 0, 0, 1):
+    myturtle.dot(5, "Black")
+    return 1
+  else:
+    myturtle.dot(5, "White")
+    return 0
+
+def playDarts(myturtle=None):
+  p1_pts = 0
+  p2_pts = 0
+  for x in range (10):
+    throwDart(myturtle)
+    if throwDart(myturtle)==1:
+      p1_pts+=1
+    throwDart(myturtle)
+    if throwDart(myturtle)==1:
+      p2_pts+=1
+  print("Player 1 Score: " + str(p1_pts))
+  print("Player 2 Score " + str(p2_pts))
+        
+def montePi(myturtle=None, num_darts=0):
+  accumulator = 0
+  total = 0
+  for x in range (num_darts):
+    throwDart(myturtle)
+    inside_circle = myturtle.distance(0,0)
+    total += 1
+    if inside_circle < 1:
+      accumulator+=1
+  estimation = (accumulator/total) * 4
+  return estimation
+
+  
 #########################################################
 #         Do not alter any code below here              #
 #       Your code must work with the main proivided     #
